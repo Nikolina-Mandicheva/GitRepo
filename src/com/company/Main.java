12aae5a48753ab83a1b1a081b0cc662ca24e8cd3
nikolina.mandicheva <nikolina.mandicheva@mentormate.com>
2022-04-05 00:18:23 +0300

    Added ignore file and introduce yourself

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println();
+        String hello="Hello World";
+        System.out.println(hello);
+        introduceYourself();
     }
+    static void introduceYourself(){
+        //Show my name
+        String firstName= new String("Nikolina");
+        String middleName= new String("Dimitrova");
+        String lastName= new String("Mandicheva");
+        System.out.println("My full name is: " + firstName + " " + middleName + " " + lastName);
+    }
+
 }
